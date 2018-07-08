8d45cb7814a8076cba870700420c4f59fdfc06b5
Andrico <andrico1234@yahoo.co.uk>
2018-07-08 12:50:18 +0100

    initial commit

---
+++ src/pages/blog.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const BlogPage = () => (
+  <div>
+    <h1>Blog Page</h1>
+    <p>Welcome to page 2</p>
+    <Link to="/">Go back to the homepage</Link>
+  </div>
+)
+
+export default BlogPage
