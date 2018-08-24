8abda36f59669be38265d78222fd16645d3b3316
Andrico <andrico1234@yahoo.co.uk>
2018-08-24 08:38:59 +0100

    fix issue with build breaking

--- packages/blog/src/pages/blogItem.js
+++ packages/blog/src/pages/blogItem.js
@@ -6,12 +6,16 @@ const StyledAnchor = styled.a`
   display: block;
 `
 
-export default ({ pathContext: { data }}) => (
-  <div>
-    <h1>{data.title}</h1>
-    <p>{data.content}</p>
-    <StyledAnchor href={data.href}>Full article</StyledAnchor>
-    <Link to="/">Back to Blog</Link>
-  </div>
-)
+export default ({ pathContext: { data }}) => {
+  if (!data) return null
+
+  return (
+    <div>
+      <h1>{data.title}</h1>
+      <p>{data.content}</p>
+      <StyledAnchor href={data.href}>Full article</StyledAnchor>
+      <Link to="/">Back to Blog</Link>
+    </div>
+  )
+}
 
--- packages/shop/src/pages/shoppingItem.js
+++ packages/shop/src/pages/shoppingItem.js
@@ -2,11 +2,15 @@ import React from 'react'
 import Link from 'gatsby-link'
 import { Button } from '@lerna-monorepo/shared-ui'
 
-export default ({ pathContext: { data }}) => (
-  <div>
-    <h1>{data.title}</h1>
-    <p>{data.content}</p>
-    <Button>Purchase</Button>
-    <Link to="/">Back to Shop</Link>
-  </div>
-)
+export default ({ pathContext: { data }}) => {
+  if (!data) return null
+
+  return (
+    <div>
+      <h1>{data.title}</h1>
+      <p>{data.content}</p>
+      <Button>Purchase</Button>
+      <Link to="/">Back to Shop</Link>
+    </div>
+  )
+}
